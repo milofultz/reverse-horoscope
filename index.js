8a192a81c368cb570eec1148c89ffa3f69d07e1b
Milo Fultz <milopfultz@gmail.com>
2020-12-24 09:33:01 -0800

    Create header

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ $(document).ready(function () {
   var $body = $('body');
 
   // Create new HTML elements
+  var $header = $('<header></header>');
+
   // Create event handler functions
+
   // Set event listeners (providing appropriate handlers as input)
+
   // Append new HTML elements to the DOM
+  $header.appendTo($body);
 });
