e57dc53b82195d354f254bb271039d0789577902
Milo Fultz <milopfultz@gmail.com>
2020-12-24 09:31:12 -0800

    Select existing elements with JS

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  // Select already existing elements
+  var $body = $('body');
+
+  // Create new HTML elements
+  // Create event handler functions
+  // Set event listeners (providing appropriate handlers as input)
+  // Append new HTML elements to the DOM
+});
