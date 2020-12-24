5f8eff41d29f1b429dc40397ff6908ac42f56d11
Milo Fultz <milopfultz@gmail.com>
2020-12-24 09:59:05 -0800

    Create title screen

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
   var $nav = $('<nav><ul class="nav-list" id="nav-list"><li class="nav-list-item" id="nav-list-about">About</li></ul></nav>');
 
   var $main = $('<main></main>');
+  var $titleScreen = $('<section class="title-screen container" id="title-screen"></section>');
+  var $mainTitle = $('<h1 class="title" id="main-title">What\'s Your Actual Sign?</h1>');
+  var $mainDescription = $('<h2 class="description" id="main-description">Discover when you were supposed to be born</h2>');
+  var $quizStartButton = $('<div class="button-container" id="quiz-start-button-container"><button class="button" id="quiz-start-button" value="Start">Start</button></div>');
 
   // Create event handler functions
 
@@ -17,4 +21,8 @@ $(document).ready(function () {
   $nav.appendTo($header);
 
   $main.appendTo($body);
+  $titleScreen.appendTo($main);
+  $mainTitle.appendTo($titleScreen);
+  $mainDescription.appendTo($titleScreen);
+  $quizStartButton.appendTo($titleScreen);
 });
