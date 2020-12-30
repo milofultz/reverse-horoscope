5c15c435b766f15500cef127d997f5b98307d030
Milo Fultz <milopfultz@gmail.com>
2020-12-30 08:38:56 -0800

    Make better buttons

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ $(document).ready(function () {
   var $titleScreen = $('<section class="container title-screen text-center" id="title-screen"></section>');
   var $mainTitle = $('<h1 class="title" id="main-title">What\'s Your<br>Actual Sign?</h1>');
   var $mainDescription = $('<h2 class="description" id="main-description">Discover when you should have been born</h2>');
-  var $quizStartButton = $('<div class="button-container" id="quiz-start-button-container"><button class="button" id="quiz-start-button" value="Start">Start</button></div>');
+  var $quizStartButton = $('<div class="button" id="quiz-start-button">Start</div>');
   // Main:Quiz
   var $quiz = $('<section class="container" id="quiz"></section>');
   var $quizForm = $('<form action="" id="quiz-form"></form>'); // all q's dynamically generated
-  var $quizSubmit = $('<div class="button-container" id="quiz-submit-button-container"><button class="button" id="quiz-submit-button" value="Submit">Submit</button></div>');
+  var $quizSubmit = $('<div class="button" id="quiz-submit-button">Submit</div>');
   // Main:Results
   var $results = $('<section class="container" id="results"></section>');
   var $resultsTitle = $('<h1 class="title" id="results-title"></h1>');
